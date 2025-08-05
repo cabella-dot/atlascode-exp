322247b4b68212bd1522aa3dfb340786f3f9330b
Christian Abella <cabella@atlassian.com>
2025-08-04 14:22:44 -1000

    Update AggClient.kt

--- intellij-extension/src/main/kotlin/com/atlassian/intellijextension/agg/AggClient.kt
+++ intellij-extension/src/main/kotlin/com/atlassian/intellijextension/agg/AggClient.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.coroutineScope
 
 class AggClient {
 	val client: ApolloClient = ApolloClient.Builder().serverUrl("https://api.atlassian.com/graphql").build()
-	val apiToken = "ATATT3xFfGF0C6a-pyyqOOAndIot6XKZQOCJNeyMC8TPWcMF-LV-omkeVSz40c8NP2EUtkA6zObPvbW6U6KYf1iCGyBB0uyj_P2HmSRwsgmiEFgJen7MM3Ith-eEgodmU5pqlT5roapEP7XOMDcTGkRoi36OpRq6nwtmwWktC5HxG-SpTY-sDD4=04A074D3"
+	val apiToken = "<api-token>"
 
 	suspend fun <D: Query.Data> executeQuery(query: Query<D>): ApolloResponse<D> {
 		val response = coroutineScope {
